6f525f33dbe2b988e891d4792e8a309491dbc74b
juliawiggeshoff <juliawiggeshoff@gmail.com>
2023-08-16 17:48:12 +0200

    minor changes to filanames and formatted code

--- scripts/pynacl_cryptography.py
+++ scripts/pynacl_cryptography.py
@@ -16,10 +16,22 @@ files_path = os.path.join(project_root, "files")
 
 # Timestamp for filenames
 dateTimeObj = datetime.datetime.now(tz=None)
-timestamp = str(dateTimeObj.year) + "-" + str(dateTimeObj.month) + "-" + str(dateTimeObj.day) + "_" + str(dateTimeObj.hour) + "-" + str(dateTimeObj.minute) + "-" + str(dateTimeObj.second)
+timestamp = (
+    str(dateTimeObj.year)
+    + "-"
+    + str(dateTimeObj.month)
+    + "-"
+    + str(dateTimeObj.day)
+    + "_"
+    + str(dateTimeObj.hour)
+    + "-"
+    + str(dateTimeObj.minute)
+    + "-"
+    + str(dateTimeObj.second)
+)
+
 
 def create_new_keypair(orig_site: str) -> IO:
-
     # Generate private key
     priv_key = PrivateKey.generate()
     # Generate matchig public key
@@ -36,22 +48,27 @@ def create_new_keypair(orig_site: str) -> IO:
     # Filename for a site's public key
     pub_filename = orig_site + ".pub"
     # Output path to a site's public key
-    pub_outpath = os.path.join(pub_keys_path, pub_filename)    
+    pub_outpath = os.path.join(pub_keys_path, pub_filename)
 
     # If files don't exist, try to write them
     if not os.path.isfile(priv_outpath):
         try:
             with open(priv_outpath, "wt") as f:
                 f.write(priv_key_b64)
-            print("File with private key for {}: {}".format(orig_site,priv_outpath))
+            print("File with private key for {}: {}".format(orig_site, priv_outpath))
         except OSError as err:
-            print("Could not write file with private key for {}: {}".format(orig_site, err))
+            print(
+                "Could not write file with private key for {}: {}".format(
+                    orig_site, err
+                )
+            )
     if not os.path.isfile(pub_outpath):
         try:
             with open(pub_outpath, "wt") as f:
                 f.write(pub_key_b64)
         except OSError as err:
-            print("Could not write public key file {}: {}".format(pub_outpath,err))
+            print("Could not write public key file {}: {}".format(pub_outpath, err))
+
 
 def import_priv_key(orig_site: str) -> bytes:
     # Filename for a site's private key
@@ -66,12 +83,13 @@ def import_priv_key(orig_site: str) -> bytes:
             with open(priv_path, "r") as f:
                 priv_key_b64_utf_8 = f.read()
             priv_key_b64_bytes = bytes(priv_key_b64_utf_8, "utf-8")
-            priv_key = PrivateKey(priv_key_b64_bytes, encoder=Base64Encoder)    
+            priv_key = PrivateKey(priv_key_b64_bytes, encoder=Base64Encoder)
 
-            return priv_key     
+            return priv_key
 
         except OSError as err:
             print("Could not open {}: {}".format(priv_path, err))
+
 
 def import_pub_key(receiv_site: str) -> bytes:
     # Filename for a site's public key
@@ -86,20 +104,23 @@ def import_pub_key(receiv_site: str) -> bytes:
             with open(pub_path, "r") as f:
                 pub_key_b64_utf_8 = f.read()
             pub_key_b64_bytes = bytes(pub_key_b64_utf_8, "utf-8")
-            pub_key = PublicKey(pub_key_b64_bytes, encoder=Base64Encoder)    
+            pub_key = PublicKey(pub_key_b64_bytes, encoder=Base64Encoder)
 
-            return pub_key     
+            return pub_key
 
         except OSError as err:
             print("Could not open {}: {}".format(pub_path, err))
 
+
 def create_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
-    #pt_id/to_receiv_site, pt_id/to_receiv_site/tmp are created by tf5t.py
+    # pt_id/to_receiv_site, pt_id/to_receiv_site/tmp are created by tf5t.py
 
     # Path to receiver's pt_id
     to_receiv_dir = os.path.join(files_path, pt_id, str("to_" + receiv_site))
     # Name from archive
-    archive_filename = pt_id + "_" + orig_site + "_" + receiv_site + "_"  +  timestamp + ".tar.gz"
+    archive_filename = (
+        pt_id + "_" + orig_site + "_" + receiv_site + "_" + timestamp + ".tar.gz"
+    )
     # Path to temp directory for pt_id
     pt_id_tmp = os.path.join(to_receiv_dir, "tmp")
     # Output path
@@ -114,10 +135,23 @@ def create_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
     if not os.path.isfile(archive_outpath):
         try:
             with tarfile.open(archive_outpath, "x:gz") as tar:
-                tar.add(to_receiv_dir, arcname=str(orig_site + "_" + receiv_site + "_" + pt_id), filter=exclude_tmp)
-            print("Created archive from {} to be sent to {}: {}".format(orig_site,receiv_site,archive_outpath))
+                tar.add(
+                    to_receiv_dir,
+                    arcname=str(orig_site + "_" + receiv_site + "_" + pt_id),
+                    filter=exclude_tmp,
+                )
+            print(
+                "Created archive from {} to be sent to {}: {}".format(
+                    orig_site, receiv_site, archive_outpath
+                )
+            )
         except OSError as err:
-            print("Failed to create archive from {} to be sent to {}: {}{}Error: {}".format(orig_site,receiv_site,archive_outpath,"\n",err))
+            print(
+                "Failed to create archive from {} to be sent to {}: {}{}Error: {}".format(
+                    orig_site, receiv_site, archive_outpath, "\n", err
+                )
+            )
+
 
 def encrypt_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
     priv_key_orig = import_priv_key(orig_site)
@@ -129,7 +163,9 @@ def encrypt_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
     orig_box = Box(priv_key_orig, pub_key_receiv)
 
     # Path to archive from origin site
-    archive_filename = pt_id + "_" + orig_site + "_" + receiv_site + "_"  +  timestamp + ".tar.gz"
+    archive_filename = (
+        pt_id + "_" + orig_site + "_" + receiv_site + "_" + timestamp + ".tar.gz"
+    )
     pt_id_tmp = os.path.join(files_path, pt_id, str("to_" + receiv_site), "tmp")
     archive_path = os.path.join(pt_id_tmp, archive_filename)
 
@@ -144,21 +180,39 @@ def encrypt_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
             # Poly1305 authentication tag preprended to the ciphertext
             # enc_archive contains the ciphertext along with the authentication info
             # and the nonce used for this encryption
-            # Encrypted archive will be exactly 40 bytes longer than the original archive  
+            # Encrypted archive will be exactly 40 bytes longer than the original archive
             enc_archive = orig_box.encrypt(archive)
 
-            enc_archive_filename = pt_id + "_" + orig_site + "_" + receiv_site + "_"  +  timestamp + ".tar.gz.enc"
-            enc_archive_path = os.path.join(pt_id_tmp,enc_archive_filename)
+            enc_archive_filename = (
+                pt_id
+                + "_"
+                + orig_site
+                + "_"
+                + receiv_site
+                + "_"
+                + timestamp
+                + ".enc"
+            )
+            enc_archive_path = os.path.join(pt_id_tmp, enc_archive_filename)
 
             if not os.path.isfile(enc_archive_path):
                 try:
                     with open(enc_archive_path, "wb") as f:
                         f.write(enc_archive)
-                    print("Successfully encrypted archive for {} from {} to be sent to {}: {}".format(pt_id,orig_site,receiv_site,enc_archive_path))
+                    print(
+                        "Encrypted archive for {} from {} to be sent to {}: {}".format(
+                            pt_id, orig_site, receiv_site, enc_archive_path
+                        )
+                    )
                 except OSError as err:
-                    print("Failed to encrypt archive for {} from {} to be sent to {}: {}{}Error: {}".format(pt_id,orig_site,receiv_site,enc_archive_path,"\n",err))
+                    print(
+                        "Failed to encrypt archive for {} from {} to be sent to {}: {}{}Error: {}".format(
+                            pt_id, orig_site, receiv_site, enc_archive_path, "\n", err
+                        )
+                    )
         except OSError as err:
-            print("Could not open archive {}: {}".format(archive_path,err))
+            print("Could not open archive {}: {}".format(archive_path, err))
+
 
 def decrypt_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
     priv_key_receiv = import_priv_key(receiv_site)
@@ -168,11 +222,13 @@ def decrypt_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
     # and the public key from the origin site
     receiv_box = Box(priv_key_receiv, pub_key_orig)
 
-    # Path to encrypted archive from origin site 
+    # Path to encrypted archive from origin site
     to_receiv_dir = os.path.join(files_path, pt_id, str("to_" + receiv_site))
     pt_id_temp = os.path.join(to_receiv_dir, "tmp")
-    enc_archive_filename = pt_id + "_" + orig_site + "_" + receiv_site + "_"  +  timestamp + ".tar.gz.enc"
-    enc_archive_path = os.path.join(pt_id_temp,enc_archive_filename)   
+    enc_archive_filename = (
+        pt_id + "_" + orig_site + "_" + receiv_site + "_" + timestamp + ".enc"
+    )
+    enc_archive_path = os.path.join(pt_id_temp, enc_archive_filename)
 
     # Open and decrypt archive
 
@@ -186,31 +242,63 @@ def decrypt_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
             dec_archive = receiv_box.decrypt(enc_archive)
 
             # Path to receiver's decrypted archive (.tar.gz)
-            dec_archive_filename = pt_id + "_" + orig_site + "_" + receiv_site + "_"  +  timestamp + ".tar.gz.dec"
-            dec_archive_path = os.path.join(to_receiv_dir,dec_archive_filename)
+            dec_archive_filename = (
+                pt_id
+                + "_"
+                + orig_site
+                + "_"
+                + receiv_site
+                + "_"
+                + timestamp
+                + ".dec.tar.gz"
+            )
+            dec_archive_path = os.path.join(to_receiv_dir, dec_archive_filename)
 
             if not os.path.isfile(dec_archive_path):
                 try:
                     with open(dec_archive_path, "wb") as f:
                         f.write(dec_archive)
-                    print("Successfully decrypted archive for {} from {} to {}: {}".format(pt_id,orig_site,receiv_site,dec_archive_path))
+                    print(
+                        "Decrypted archive for {} from {} to {}: {}".format(
+                            pt_id, orig_site, receiv_site, dec_archive_path
+                        )
+                    )
                 except OSError as err:
-                    print("Failed to create decrypted archive for {} from {} to be sent to {}: {}{}Error: {}".format(pt_id,orig_site,receiv_site,enc_archive_path,"\n",err))
+                    print(
+                        "Failed to create decrypted archive for {} from {} to be sent to {}: {}{}Error: {}".format(
+                            pt_id, orig_site, receiv_site, enc_archive_path, "\n", err
+                        )
+                    )
         except OSError as err:
-            print("Failed to open encrypted archive {}: {}".format(enc_archive_path,err))
+            print(
+                "Failed to open encrypted archive {}: {}".format(enc_archive_path, err)
+            )
+
 
 def unpack_archive(pt_id: str, orig_site: str, receiv_site: str) -> IO:
     # Path to receiver's decrypted archive (.tar.gz)
     to_receiv_dir = os.path.join(files_path, pt_id, str("to_" + receiv_site))
-    dec_archive_filename = pt_id + "_" + orig_site + "_" + receiv_site + "_"  +  timestamp + ".tar.gz.dec"
-    dec_archive_path = os.path.join(to_receiv_dir,dec_archive_filename)    
+    dec_archive_filename = (
+        pt_id + "_" + orig_site + "_" + receiv_site + "_" + timestamp + ".dec.tar.gz"
+    )
+    dec_archive_path = os.path.join(to_receiv_dir, dec_archive_filename)
 
-    unpacked_archive_path = os.path.join(to_receiv_dir, str(orig_site + "_" + receiv_site + "_" + pt_id))
+    unpacked_archive_path = os.path.join(
+        to_receiv_dir, str(orig_site + "_" + receiv_site + "_" + pt_id)
+    )
 
     if os.path.isfile(dec_archive_path):
         try:
             with tarfile.open(dec_archive_path, "r:gz") as tar:
                 tar.extractall(path=to_receiv_dir)
-            print("Unpacked archive from {} sent to {}: {}".format(orig_site,receiv_site,unpacked_archive_path))
+            print(
+                "Unpacked archive from {} sent to {}: {}".format(
+                    orig_site, receiv_site, unpacked_archive_path
+                )
+            )
         except OSError as err:
-                print("Failed to open archive from {} sent to {}: {}{}Error: {}".format(orig_site,receiv_site,dec_archive_path,"\n",err))
+            print(
+                "Failed to open archive from {} sent to {}: {}{}Error: {}".format(
+                    orig_site, receiv_site, dec_archive_path, "\n", err
+                )
+            )
